668c26e66ac479a3ea14eaecc0c494b1702d5244
kimhyemin <www_wowwh@naver.com>
2023-09-20 13:41:13 +0900

    9/20

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -7,13 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.function.Supplier;
+
+import javax.transaction.Transactional;
 
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
@@ -25,16 +30,42 @@ public class DummyControllerTest {
 	@Autowired //의존성 주입 
 	private UserRepository userRepository;
 	
-	//http://localhost:8000/blog/dummy/user //에러발
-	@GetMapping("/dummy/user")
+	//save 함수는 id를 전달하지 않으면 insert 해주고
+	//save 함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해
+	//save 함수는 id를 전달하면 당 id에 대한 데이터가 없으며 inser를 
+	//email, password update
+	@Transactional
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id,@RequestBody User requestUser) {
+		//json 데이터를 요청 => Java object(MessageConverter의 jackson라이브러리가 변환해서 받아줌
+		System.out.println("id : " + id);
+		System.out.println("password : " + requestUser.getPassword());
+		System.out.println("email : " + requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+		//userRepository.save(user);
+		//더티 체킹 
+		return null;
+	}
+	
+	//http://localhost:8000/blog/dummy/user //에러발생 
+	@GetMapping("/dummy/users")
 	public List<User> list(){
 		return userRepository.findAll();
 	}
 	
 	//한페이지당 2건에 데이터를 리턴받아 볼 예정
-	@GetMapping("/dummy/user/page")
-	public Page<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC)Pageable pageable){
-		Page<User> users = userRepository.findAll(pageable);
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC)Pageable pageable){
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+	
+		List<User> users = pagingUser.getContent();
 		return users;
 	}
 
