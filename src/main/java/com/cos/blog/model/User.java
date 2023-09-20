1092301b359289ededfa64692be31575ab86f584
kimhyemin <www_wowwh@naver.com>
2023-09-20 12:09:11 +0900

    9/20

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -4,12 +4,15 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,6 +24,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 @Entity //user class가 자동으로 MySQL에 테이블이 생성이 된다.
+//@DynamicInsert //insert시에 null인 필드를 제외시켜준다.
 public class User {
 	@Id //primary key
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
@@ -35,8 +39,10 @@ public class User {
 	@Column(nullable = false, length = 50)
 	private String email;
 	
-	@ColumnDefault("'user'")
-	private String role; //Enum을 쓰는게 좋다. , Admin(관리자), user, manager 3개의 권한을 주는.. 
+	//DB는 RoleType이라는게 없다.
+	//@ColumnDefault("'user'")
+	@Enumerated(EnumType.STRING)
+	private RoleType role; //Enum을 쓰는게 좋다. , ADMIN(관리자), USER, MANAGER 3개의 권한을 주는.. 
 	
 	@CreationTimestamp //시간이 자동 입력 
 	private Timestamp createDate; //회원이 가입한 시간 
--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,19 +1,66 @@
 package com.cos.blog.test;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.function.Supplier;
 
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
-
+//html파일이 아니라 data를 리턴해주는 controller = RestController
 @RestController
 public class DummyControllerTest {
 	
 	@Autowired //의존성 주입 
 	private UserRepository userRepository;
 	
+	//http://localhost:8000/blog/dummy/user //에러발
+	@GetMapping("/dummy/user")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	//한페이지당 2건에 데이터를 리턴받아 볼 예정
+	@GetMapping("/dummy/user/page")
+	public Page<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC)Pageable pageable){
+		Page<User> users = userRepository.findAll(pageable);
+		return users;
+	}
+
+	//{id} 주소로 파마레터를 전달 받을 수 있음
+	//http://localhost:8000/blog/dummy/user/3
+
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		//user/4을 찾으면 내가 데이터베이스에서 못찾아오게 되면 user가 null이 될 것 아니냐?
+		//그럼 return null 이 리턴이 되잖아 그럼 프로그램에 문제가 있지 않겠니 ?
+		//Optional로 너의 User 객체를 감싸서 가져올테니 null인지 아닌지 판단해서 return 해 !
+	    User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+	    	@Override
+	    	public IllegalArgumentException get() {
+	    		return new IllegalArgumentException("해당유저는 없습니다. id : " +id);
+	    	}
+	    });
+	    //요청 = 웹 브라우
+	    //user 객체 = java object
+	    //변환(웹브라우저가 이해할 수 있는 데이터) -> json(Gson 라이브러리) 
+	    //스프링 부트 = MessageConverter 라는 애가 응답시에 자동 작동
+	    //만약에 자바 오브젝트를 리턴하게 되면 MseeageConverter가 Jackson 라이브러리를 호출해서
+	    //user 오브젝트를 json으로 변환해서 브라우저에게 던져 준다.
+	   return user;
+	}
+
 	//http://localhost:8000/blog/dummy/join (요청)
 	//http의 body에 username, password, email 데이터를 가지고 (요청) 
 	@PostMapping("/dummy/join")
@@ -25,6 +72,9 @@ public class DummyControllerTest {
 		System.out.println("role : " + user.getRole());
 		System.out.println("createDate : " +user.getCreateDate());
 		
+		user.setRole(RoleType.USER);
+		//user.setRole(RoleType.USER.toString());
+
 		userRepository.save(user);
 		return "회원가입이 완료되었습니다.";
 	}
