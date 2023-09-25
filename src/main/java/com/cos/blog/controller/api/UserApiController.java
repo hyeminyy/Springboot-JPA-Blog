84c7057c1d4f3b987150ac5cf70dd60c913b453f
kimhyemin <www_wowwh@naver.com>
2023-09-25 20:48:51 +0900

    9/25

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,5 +1,8 @@
 package com.cos.blog.controller.api;
 
+import javax.servlet.http.HttpSession;
+
+import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +20,9 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private HttpSession session;
+	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) { //username, password, email
 		System.out.println("UserApiController :save 호출됨");
@@ -25,4 +31,16 @@ public class UserApiController {
 		userService.회원가입(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
+	
+	@PostMapping("/api/user/login")
+	public ResponseDto<Integer> login(@RequestBody User user){
+		System.out.println("UserApiController :login 호출됨");
+		User principal = userService.로그인(user); //principal (접근주체) 
+		
+		if(principal != null) {
+			session.setAttribute("principal", principal);
+		}
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	}
+	
 }
