e29232f05e00bf5879e7ff0aed4a6d17358829b7
kimhyemin <www_wowwh@naver.com>
2023-09-28 02:32:27 +0900

    9/28

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,13 +1,20 @@
 package com.cos.blog.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.cos.blog.auth.PrincipalDetail;
 import com.cos.blog.service.BoardService;
+
 
 @Controller
 public class BoardController {
@@ -16,9 +23,15 @@ public class BoardController {
 	private BoardService boardService;
 	
 	@GetMapping({"", "/"})
-	public String index(Model model) {
-		model.addAttribute("boards", boardService.글목록());
+	public String index(Model model, @PageableDefault(size = 3, sort= "id", direction = Sort.Direction.DESC)Pageable pageable) {
+		model.addAttribute("boards", boardService.글목록(pageable));
 		return "index"; //viewResolver 작동 !!
+	}
+	
+	@GetMapping("/board/{id}")
+	public String findById(@PathVariable int id,Model model) {
+		model.addAttribute("board", boardService.글상세보기(id));
+	 return "board/detail";
 	}
 	
 	//user 권한이 필요 
