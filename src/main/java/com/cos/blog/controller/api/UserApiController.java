0e503032322958f5baf7eb4ce4595109746d57c2
kimhyemin <www_wowwh@naver.com>
2023-09-29 00:40:41 +0900

    9/29

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +31,12 @@ public class UserApiController {
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 	
+	@PutMapping("/user")
+	public ResponseDto<Integer> update(@RequestBody User user){ //key = value, x-www-form-urlencoded
+		userService.회원수정(user);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+		
+	}
 	
 	
 	
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,6 +3,9 @@ let index = {
 			$("#btn-save").on("click", ()=>{ // function(){} , ()=>{} this를 바인딩하기 위해서!! 
 				this.save();
 			});
+			$("#btn-update").on("click", ()=>{ // function(){} , ()=>{} this를 바인딩하기 위해서!! 
+				this.update();
+			});
   },
 
   save: function(){
@@ -13,11 +16,6 @@ let index = {
 					email: $("#email").val()
 			};
 	
-	//console.log(data);
-	
-	//ajax 호출 시 default가 비동기호출 
-	//ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청 !
-	//ajax가 통신을 성공하고 서버가 json을 리턴해주면 자동으로 자바 오브젝트로 변환해줌 
 	$.ajax({
 		//회원가입 수행 요청 
 		type : "POST",
@@ -28,6 +26,33 @@ let index = {
 		
 	}).done(function(resp){
 		alert("회원가입이 완료되었습니다.");
+		console.log(resp);
+		location.href="/";
+	}).fail(function(error){
+					alert(JSON.stringify(error));
+				}); //ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청 
+	
+  },
+  
+  update: function(){
+			//alert('user의 save함수 호출됨');
+			let data = {
+					id: $("#id").val(),
+					password: $("#password").val(),
+					email: $("#email").val()
+			};
+	
+
+	$.ajax({
+		//회원가입 수행 요청 
+		type : "PUT",
+		url: "/user",
+		data:JSON.stringify(data), //http body 데이터 
+		contentType: "application/json; charset=utf-8", //body 데이터가 어떤 타입인지 (mime) 
+		dataType : "json" //요청을 서버로해서 응답이 왔을 때 기본적으로 모든것이 문자열(생긴게 json이라면 => javascript )
+		
+	}).done(function(resp){
+		alert("회원수정이 완료되었습니다.");
 		console.log(resp);
 		location.href="/";
 	}).fail(function(error){
